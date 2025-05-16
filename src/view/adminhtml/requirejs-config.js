0fbfc18d6a7a2ff6b28d61bdae5a79237ea8e3fe
Willem Poortman <wp@hyva.io>
2025-05-16 12:36:38 +0200

    WIP

--- src/view/adminhtml/requirejs-config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-    deps: [
-        'Magewirephp_MagewireAdmin/page/js/magewire/features/support-magento-flash-messages/support-magento-flash-messages'
-    ]
-};
--- src/view/adminhtml/web/page/js/magewire/features/support-magento-flash-messages/support-magento-flash-messages.js
+++
@@ -1,9 +0,0 @@
-define([
-    'mage/translate'
-], function (uiRegistry, $t) {
-    'use strict';
-
-    window.addEventListener('magewire:flash-messages:dispatch', function (event) {
-        console.log('WIP: flash messages support has not yet been build.', event.detail)
-    });
-});
