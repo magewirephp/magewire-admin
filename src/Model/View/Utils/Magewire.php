b333279c3601e1c8b288e65dfe96fe5d8c9f39ba
Michiel Gerritsen <michiel@controlaltdelete.nl>
2025-05-23 10:27:51 +0200

    Bugfix: Make admin URL dynamic

--- src/Model/View/Utils/Magewire.php
+++ src/Model/View/Utils/Magewire.php
@@ -10,10 +10,29 @@ declare(strict_types=1);
 
 namespace Magewirephp\MagewireAdmin\Model\View\Utils;
 
+use Magento\Backend\Setup\ConfigOptionsList as BackendConfigOptionsList;
+use Magento\Framework\App\DeploymentConfig;
+use Magewirephp\Magewire\MagewireServiceProvider;
+use Magewirephp\Magewire\Model\Magento\System\ConfigMagewire as MagewireSystemConfig;
+use Magewirephp\Magewire\Model\View\Utils\Magewire\Features as FeaturesViewUtil;
+use Magewirephp\Magewire\Model\View\Utils\Magewire\Mechanisms as MechanismsViewUtil;
+
 class Magewire extends \Magewirephp\Magewire\Model\View\Utils\Magewire
 {
+    public function __construct(
+        FeaturesViewUtil $features,
+        MechanismsViewUtil $mechanisms,
+        MagewireServiceProvider $magewireServiceProvider,
+        MagewireSystemConfig $config,
+        private readonly DeploymentConfig $deploymentConfig,
+    ) {
+        parent::__construct($features, $mechanisms, $magewireServiceProvider, $config);
+    }
+
     public function getUpdateUri(): string
     {
-        return '/backend' . parent::getUpdateUri();
+        return '/' .
+            $this->deploymentConfig->get(BackendConfigOptionsList::CONFIG_PATH_BACKEND_FRONTNAME) .
+            parent::getUpdateUri();
     }
 }
